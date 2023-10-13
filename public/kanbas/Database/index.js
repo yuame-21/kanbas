a345b47cf53beae0f7271abb8be6a13f6454966e
yuame-21 <yu.ame@northeastern.edu>
2023-10-13 00:31:00 -0400

    start a3

---
+++ public/kanbas/Database/index.js
@@ -0,0 +1,7 @@
+import courses from "./courses.json";
+
+// if name = content then we can just make courses: courses to courses
+
+export default {
+  courses
+};
---
+++ public/kanbas/index.js
@@ -0,0 +1,16 @@
+function Kanbas() {
+    return (
+        <div className="d-flex">
+            <div>
+                <h1>Kanbas Navigation</h1>
+            </div>
+            <div>
+                <h1>Account</h1>
+                <h1>Dashboard</h1>
+                <h1>Courses</h1>
+            </div>
+        </div>
+    );
+}
+export default Kanbas;
+
