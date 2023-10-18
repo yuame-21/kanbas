5a086310fe24d5c881568dd838405c5725eafa77
yuame-21 <yu.ame@northeastern.edu>
2023-10-18 00:59:43 -0400

    nav bar finished?

---
+++ src/Kanbas/KanbasNavigation/index.js
@@ -0,0 +1,51 @@
+import './styles.css';
+
+import { Link, useLocation } from "react-router-dom";
+import NEULogo from '../../Images/NEU_Logo.jpg';
+import {FaUserLarge, FaGauge, FaAddressBook, FaCalendarDays,
+    FaInbox, FaClock, FaTv, FaArrowRightFromBracket, FaRegCircleQuestion} from "react-icons/fa6";
+
+
+function KanbasNavigation() {
+    const links = [{name: "Account", iconClass: "FaUserLarge"},
+        {name: "Dashboard", iconClass: "FaGauge"},
+        {name: "Courses", iconClass: "FaAddressBook"},
+        {name: "Calendar", iconClass: "FaCalendarDays"},
+        {name: "Inbox", iconClass: "FaInbox"},
+        {name: "History", iconClass: "FaClock"},
+        {name: "Studio", iconClass: "FaTv"},
+        {name: "Commons", iconClass: "FaArrowRightFromBracket"},
+        {name: "Help", iconClass: "FaRegCircleQuestion"}];
+    const { pathname } = useLocation();
+    return (
+        <div className="wd-navbar-container">
+        <ul className="wd-navbar-style">
+            <li className="list-group-item">
+                <img class="wd-logo" src={NEULogo} alt="Northeastern Logo"/>
+            </li>
+            {links.map((link, index) => (
+                <Link
+                    key={index}
+                    to={`/Kanbas/${link.name}`}
+                    className={`list-group-item ${pathname.includes(link.name) && "active"}`}>
+                    {link.name}
+                    <div>
+                    {link.iconClass === "FaUserLarge" && <FaUserLarge style={{}}/>}
+                    {link.iconClass === "FaGauge" && <FaGauge/>}
+                    {link.iconClass === "FaAddressBook" && <FaAddressBook/>}
+                    {link.iconClass === "FaCalendarDays" && <FaCalendarDays/>}
+                    {link.iconClass === "FaInbox" && <FaInbox/>}
+                    {link.iconClass === "FaClock" && <FaClock/>}
+                    {link.iconClass === "FaTv" && <FaTv/>}
+                    {link.iconClass === "FaArrowRightFromBracket" && <FaArrowRightFromBracket/>}
+                    {link.iconClass === "FaRegCircleQuestion" && <FaRegCircleQuestion/>}
+                    </div>
+                </Link>
+
+            ))}
+        </ul>
+        </div>
+    );
+}
+export default KanbasNavigation;
+
--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,12 +1,18 @@
-import Nav from "../Nav";
+import KanbasNavigation from "./KanbasNavigation";
 
 function Kanbas() {
-    return(
-        <div>
-        <Nav/>
-            <h1>Kanbas</h1>
+    return (
+        <div className="d-flex">
+            <div>
+                <KanbasNavigation/>
+            </div>
+            <div>
+                <h1>Account</h1>
+                <h1>Dashboard</h1>
+                <h1>Courses</h1>
+            </div>
         </div>
     );
 }
-export default Kanbas
+export default Kanbas;
 
